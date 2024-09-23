224183eabfccd8e572b0216249926436c54cd6f0
antialberteinstein <antialberteinstein@gmail.com>
2024-09-23 19:14:59 +0700

    Add sqlite3 support

---
+++ include/db_connector.h
@@ -0,0 +1,9 @@
+#ifndef PBL_DB_CONNECTOR_H
+#define PBL_DB_CONNECTOR_H
+
+#include <iostream>
+#include <sql.h>
+
+using namespace std;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "main.h"
 #include "tui.h"
- 
+#include "db_connector.h"
+
 int main(void) {
     tui::init();
     auto screen = ScreenInteractive::TerminalOutput();
 
     tui::my_main(screen);
     tui::cleanup();
+
     return 0;
 }
